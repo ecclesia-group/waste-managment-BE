71c70231f68f9f685e29911150a1f542595f24de
wae57 <courage.ahorttor@gwosevo.com>
2025-11-26 16:41:30 +0000

    changes made

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:api');
+
+
+Route::get("yes", function(){
+    return "yes yes";
+});
