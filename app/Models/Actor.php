f7cfacac592b215332f72cd7a111ebf1981dacb4
wae57 <courage.ahorttor@gwosevo.com>
2025-11-27 15:25:44 +0000

    changes made

--- app/Models/Actor.php
+++ app/Models/Actor.php
@@ -1,17 +1,21 @@
 <?php
 namespace App\Models;
 
+use App\Traits\Helpers;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Passport\Contracts\OAuthenticatable;
 use Laravel\Passport\HasApiTokens;
 
-class Actor extends Model
+class Actor extends Authenticatable implements MustVerifyEmail, OAuthenticatable
 {
-    use HasFactory, HasApiTokens, Notifiable;
+    use HasFactory, SoftDeletes, HasApiTokens, Notifiable, Helpers;
 
-    // public function hasVerifiedEmail()
-    // {
-    //     return $this->email_verified_at != null;
-    // }
+
+
+
 }
