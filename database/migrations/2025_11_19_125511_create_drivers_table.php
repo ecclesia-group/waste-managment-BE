5868a88431850a597ec539cffe172e26eaf1f7dd
Thomas <kankamthomas6@gmail.com>
2026-01-14 14:34:43 +0000

    feat: fixed email params error and driver migration with profile image field not there

--- database/migrations/2025_11_19_125511_create_drivers_table.php
+++ database/migrations/2025_11_19_125511_create_drivers_table.php
@@ -33,6 +33,7 @@ return new class extends Migration
             $table->string('emergency_contract_address')->nullable();
             $table->longText('license_front_image')->nullable();
             $table->longText('license_back_image')->nullable();
+            $table->longText('profile_image')->nullable();
             $table->string('status')->default('active');
             $table->softDeletes();
             $table->timestamps();
